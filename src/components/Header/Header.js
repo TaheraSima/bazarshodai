d82394ea27201a4d5764d8a6592bc1559f2919f5
mthila696 <mithila@simecsystem.com>
2022-01-20 17:41:56 +0600

    search bar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+import {faCartPlus, faSearch} from '@fortawesome/free-solid-svg-icons'
+import './Header.css'
 
 function Header() {
     return (
         <div className="bg-color-green">
             <div className="row">
-                <div className="col-md-6">
+                <div className="col-md-2">
                     <div className="p-3">
                         <FontAwesomeIcon icon={faCartPlus} className="text-white" />
-                        <span className="text-white text-uppercase m-5">Bazar</span>
-                        <h6 className="text-white text-uppercase letter-space-12 p-0 m-5">Shodai</h6>
+                        <span className="text-white text-uppercase m-1">Bazar</span>
+                        <h6 className="text-white text-uppercase letter-space-12 ">Shodai</h6>
                     </div>
                 </div>
                 <div className="col-md-6">
-                   {/* <div className="input-group">
-                        <div className="form-outline">
-                            <input type="search" id="form1" className="form-control"/>
-                            <label className="form-label" htmlFor="form1">Search</label>
+                    <div className="container p-3">
+                        <div className="row height d-flex justify-content-center align-items-center">
+                            <div className="col-md-8">
+                                <div className="search">
+                                    <input type="text" className="form-control" placeholder="Search for products (eg: fish, oil)"/>
+                                    <FontAwesomeIcon icon={faSearch} />
+                                </div>
+                            </div>
                         </div>
-                        <button type="button" className="btn btn-primary">
-                            <i className="fas fa-search"></i>
-                        </button>
-                    </div>*/}
+                    </div>
+                </div>
+
+                <div className="col-md-4">
+
                 </div>
 
             </div>
